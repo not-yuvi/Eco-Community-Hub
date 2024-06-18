ff5da7c70d68d06a6b62de1394ac8e4090b57c59
Yuvraj Arora <arorayuvraj24@gmail.com>
2024-06-17 22:46:04 -0500

    add tip

--- src/components/Tips.js
+++ src/components/Tips.js
@@ -16,6 +16,14 @@ const Tips = () => {
         <h3>Tip 3: Plant Trees</h3>
         <p>Plant trees and support reforestation efforts to combat climate change.</p>
       </div>
+      <div className="tip">
+        <h3>Tip 4: Conserve Water</h3>
+        <p>Fix leaks and use water-saving appliances to conserve water resources.</p>
+      </div>
+      <div className="tip">
+        <h3>Tip 5: Reduce Food Waste</h3>
+        <p>Plan meals, store food properly, and compost waste to reduce food waste.</p>
+      </div>
       {/* Add more tips */}
     </div>
   );
